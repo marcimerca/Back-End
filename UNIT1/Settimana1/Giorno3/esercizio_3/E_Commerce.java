bab82d18da5a107315ee3c8365f4ef1cf876b589
Marcello <marcello.mercanzin@gmail.com>
2024-04-18 09:17:42 +0200

    concluso esercizio

--- UNIT1/Settimana1/Giorno3/esercizio_3/E_Commerce.java
+++ UNIT1/Settimana1/Giorno3/esercizio_3/E_Commerce.java
@@ -8,6 +8,28 @@ public class E_Commerce {
 
         Articolo articolo1 = new Articolo("11111", "Cassa bluetooth", 3, 2);
 
+        Articolo articolo2 = new Articolo("11341", "iPhone", 20, 4);
+
+
+        Articolo articolo3 = new Articolo("13441", "Tv Lg", 100, 2);
+
+        Articolo[] articoli = new Articolo[3];
+
+        articoli[0] = articolo1;
+        articoli[1] = articolo2;
+        articoli[2] = articolo3;
+
+        Carrello carrello = new Carrello(cliente1);
+
+        carrello.setArticoli(articoli);
+
+        System.out.println(carrello.getTotCostoArticoli());
+
+
+
+
+
+
 
     }
 
